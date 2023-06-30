aae6cea70bbf4e377510cbae2c2f5138ad0aad9c
Guillaume-lalib <guillaume.lalib@gmail.com>
2023-06-30 11:50:32 +0200

    update components

--- frontend/src/components/account/accountProfil.js
+++ frontend/src/components/account/accountProfil.js
@@ -1,39 +1,41 @@
 import React from 'react';
 
 const accountProfil = () => {
+  const dataAccount = [
+    {
+      id: 1,
+      account: 'Argent Bank Checking (x8349)',
+      amount: '$2,082.79',
+      description: 'Available Balance',
+    },
+    {
+      id: 2,
+      account: 'Argent Bank Savings (x6712)',
+      amount: '$10,928.42',
+      description: 'Available Balance',
+    },
+    {
+      id: 3,
+      account: 'Argent Bank Credit Card (x8349)',
+      amount: '$184.30',
+      description: 'Current Balance',
+    },
+  ];
   return (
     <section className="account-profil">
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
+      {dataAccount.map((data) => (
+        <div key={data.id} className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">{data.account}</h3>
+            <p className="account-amount">{data.amount}</p>
+            <p className="account-amount-description">{data.description}</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
         </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      ))}
     </section>
   );
 };
--- frontend/src/components/features/feet.js
+++ frontend/src/components/features/feet.js
@@ -3,30 +3,39 @@ import Chat from '../../image/icon-chat.png';
 import Money from '../../image/icon-money.png';
 import Security from '../../image/icon-security.png';
 const feet = () => {
+  const dataHomePage = [
+    {
+      id: 1,
+      title: 'You are our #1 priority',
+      content:
+        'Need to talk a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+      image: Chat,
+    },
+    {
+      id: 2,
+      title: 'More savings means higher rates',
+      content:
+        'The more you save with us, the higher your interest rate will be!',
+      image: Money,
+    },
+    {
+      id: 3,
+      title: 'Security you can trust',
+      content:
+        'We use top of the line encryption to make sure your data and money is always safe.',
+      image: Security,
+    },
+  ];
   return (
     <section className="features">
       <h2 className="sr-only">Features</h2>
-      <div className="feature-item">
-        <img src={Chat} alt="Chat Icon" className="feature-icon" />
-        <h3 className="feature-item-title">You are our #1 priority</h3>
-        <p>
-          Need to talk to a representative? You can get in touch through our
-          24/7 chat or through a phone call in less than 5 minutes.
-        </p>
-      </div>
-      <div className="feature-item">
-        <img src={Money} alt="Chat Icon" className="feature-icon" />
-        <h3 className="feature-item-title">More savings means higher rates</h3>
-        <p>The more you save with us, the higher your interest rate will be!</p>
-      </div>
-      <div className="feature-item">
-        <img src={Security} alt="Chat Icon" className="feature-icon" />
-        <h3 className="feature-item-title">Security you can trust</h3>
-        <p>
-          We use top of the line encryption to make sure your data and money is
-          always safe.
-        </p>
-      </div>
+      {dataHomePage.map((data) => (
+        <div key={data.id} className="feature-item">
+          <img src={data.image} alt="Chat Icon" className="feature-icon" />
+          <h3 className="feature-item-title">{data.title}</h3>
+          <p>{data.content}</p>
+        </div>
+      ))}
     </section>
   );
 };
